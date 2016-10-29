59c18d6dca9fb0ceffa39f41f0b7f92ca3e0633f
市川裕太 <PCyuta@ichikawayuuta-no-MacBook-Pro.local>
2016-10-29 22:21:28 +0900

    first commit backup

---
+++ talk.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <link href="reset.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <title>LINE</title>
+  </head>
+  <body>
+    <header class="line_header">
+      <div class="line_logo">
+        LINE
+        <img class="line_config" src="config.png">
+      </div>
+      <div class="line_functionChangeButton">
+        <img class="line_functionBtn1" src="personLogo.png">
+        <img class="line_functionBtn2" src="talkLogo.png">
+        <img class="line_functionBtn3" src="presonAddLogo.png">
+        <img class="line_functionBtn4" src="timelineLogo.png">
+      </div>
+    </header>
+
+    <div class="line_talkMain">
+      
+      <div class="line_searchBox">
+        <!-- <img class="line_searchLogo" src="searchLogo.png"> -->
+        <input class="line_searchInput" type="text" placeholder="トークルームとメッセージ検索">
+      </div>
+
+      <div class="line_talkBox">
+        <img class="line_picPreson" src="pic1.jpg">
+        <p class="line_showUserName">AAA</p>
+        <p class="line_message">BBB</p>
+
+        <p class="line_sentTime">uuu</p>
+      </div>
+    </div>
+
+  </body>
+</html>
